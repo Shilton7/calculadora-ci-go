901a73365b80a31a3f107724e11a69d15120614f
Shilton <shilton.sistemas@gmail.com>
2022-01-19 21:27:53 -0300

    feat: Github actions - FAIL

--- math.go
+++ math.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Soma(10, 10))
+	fmt.Println(SomaFail(10, 10))
 }
 
-func Soma(a int, b int) int {
+func SomaFail(a int, b int) int {
 	return a + b
 }
